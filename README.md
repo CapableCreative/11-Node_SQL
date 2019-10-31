a4e753ee27a1ea582dbb1e60b9cd4b2c4493a457
Stephen Fox <stephen@capablecreative.com>
2019-10-31 16:34:30 -0500

    Addition of comments and README.md - SLF

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# 11-Node_SQL
-Homework 11 - MySQL + Node Trilogy Education / Vanderbilt FSF Bootcamp
+# Homework 12: Node_SQL
+Student: <b>Stephen Fox</b>
+Trilogy Education / Vanderbilt FSF Bootcamp — July 2019 Cohort
+
+----------------
+<h4>I. The Problem</h4>
+Data is at the center of every interaction between a customer (user) and a company (provider of good or service). We need to create a rudimentary application capable of allowing those standard transations to update the database. We're looking to allow:
+<ul>
+    <li>A company to:
+        <ul>
+            <li>Create an item (POST, within the app)</li>
+            <li>Observe depletion of purchased items</li>
+            <li>Restock depleted items</li>
+        </ul>
+    <li>A customer to:
+        <ul>
+            <li>View available items</li>
+            <li>Bid on / purchase items</li>
+            <li>Recieve feedback if an item is sold out</li>
+        </ul>
+    </li>
+</ul>
+
+<div style="background: #eaeaea; padding: 5px 20px; margin: 0px 0px 0px 0px;">
+<h4>II. The Purpose</h4>
+Data is at the center of every interaction between a customer (user) and a company (provider of good or service). We need to create a rudimentary application capable of allowing those standard transations to update the database. We're looking to allow:
+<ul>
+    <li>Demonstrate an understanding of:
+    <ol>
+        <li><b>Node</b> - as a js runtime capable of CRUD database manipulations</li>
+        <li><b>SQL</b> - as a standard for database interactions</li>
+        <li><b>NPM</b> - as a package manager for node</li>
+        <li><b>NPM MySQL</b> - as a Node package capable of delivering SQL commands based on contextualized user input</li>
+        <li><b>Inquirer</b> - as a Node package capable of prompting users to provide input</li>
+    </ol>
+    <li>Execute consistency and quality while:
+    <ol>
+        <li><b>Maintaining the repo</b> - creating a verifiable record of commits and commit messages</li>
+        <li><b>README.md</b> - documenting the purpose, approach, and outcome of  the project in a clear, easy to understand manner</li>
+        <li><b>Comments</b> - authoring strategic comments throughout code to allow easy application maintenance and understanding</li>
+    </ol>
+</div>
+
+-------------
+<h4>III. Approach</h4>
+<b>I began with the GreatBay class activity</b> from a few weeks ago. Since I'm a bit behind, I leveraged quite a bit of the base of that code to inform my approach with this project. This included:<br>
+
+* Using INQUIRER npm to gather user input via prompts
+* Using MySQL npm to interact with a local database
+
+Once I understood the methodology within that application, I rewrote all the functions to allow depletion of stock from a new QUANTITY column within the table. I created a new RESTOCK option, as well as depricating the QUANTITY of items as they were purchased.
+
+While I referenced GreatBay.js, I ultimately rewrote everything to ensure I understood syntax and the methodology of the two npms used. Specific notes on my thought process are within the .js file.
+
+-------------
+
+<div style="background: #eaeaea; padding: 5px 20px; margin: 0px 0px 0px 0px;">
+<h4>IV. Useful Project Links</h4>
+
+<b>Github Repo:</b> https://github.com/CapableCreative/11-Node_SQL
+</div>
--- bamazon.js
+++ bamazon.js
@@ -1,6 +1,8 @@
+// Begin by requiring the specific npm packages below:
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// Reference MySql npm documentation to understand the parameters needed to connect to your server. This application relies on a localhost server called "homework11_db"
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,12 +11,14 @@ var connection = mysql.createConnection({
     database: "homework11_db"
 });
 
+// Use the npm built in functions to deliver error logging if a connection error occurs, otherwise, display port and instance of connection
 connection.connect(function(err){
     if (err) throw err;
     console.log("Connected as id: " +connection.threadId);
     start();
 });
 
+// start() is the main function displaying the entry-point prompt via inquirer. From here, we simply create options to lead into our other options: Post an item, Bid on an item, Quit, or Restock an item
 function start() {
     inquirer
         .prompt({
@@ -35,6 +39,7 @@ function start() {
     });
 }
 
+// if POST is selected from the start(), this function guides the user through several criteria necessary to create an item within the auctionItems table (within the homework11_db)
 function postAuction() {
     inquirer
         .prompt([
@@ -57,6 +62,7 @@ function postAuction() {
                 name: "startingBid",
                 type: "input",
                 message: "What would you like the starting bid to be? $",
+                // Simple validation to prevent empty data submission for the starting Bid — I did not consistently implement this; this is something I saw in the initial GreatBay.js and added it here just to have it
                 validate: function(value) {
                     if (isNaN(value) === false) {
                         return true;
@@ -65,6 +71,8 @@ function postAuction() {
                 }
             }
         ])
+        // Once selections have been made via the inquirer prompts, this function takes the answer key value pairs and uses them to rewrite SQL
+        // "?" are used to allow various parameters to populate along with other SQL syntax elements
         .then(function(answer) {
             connection.query("INSERT INTO auctionItems SET ?", 
             {
@@ -74,24 +82,30 @@ function postAuction() {
                 highestBid: answer.highestBid || 0,
                 quantity: answer.quantity || 0
             },
+            // If all successfully updated the database.table, this function runs to throw errors, or post success
             function(err) {
                 if (err) throw err;
                 console.log("Your new item has been posted for sale.");
+                // Once the function is complete, we call the initial start() to exit this function.
                 start();
             }
         );
     });
 }
 
+// if BID is selected from the start(), this function guides the user through several criteria necessary to bid on item within the auctionItems table (within the homework11_db)
 function bidAuction(){
+    // here we view all rows from the auctionItems table
     connection.query("SELECT * FROM auctionItems", function(err,results) {
         if (err) throw err;
         console.log(results);
+        // This logic is very similar to the POST function, but requires querying the database before action is taken
         inquirer.prompt([{
             name: "choice",
             type: "rawlist",
             choices: function(){
                 var choiceArray = [];
+                // we use a for loop to display itemName values within that column; this represents items that can be BID on
                 for (var i = 0; i < results.length; i++ ){
                     choiceArray.push(results[i].itemName);
                 } 
@@ -111,10 +125,12 @@ function bidAuction(){
                     chosenItem = results[i];
                 }
             }
-            
+
+            // Once a bid has been made, I created several variables for simpler reference within the logged items and interactions (It turns out I didn't need to parse several of them ... no harm though)
             let parsedHighestBid = parseInt(chosenItem.highestBid);
             let parsedBid = parseInt(answer.bid);
             let parsedQuantity = parseInt(chosenItem.quantity);
+            // If an item is purchased, the QUANTITY is reduced by 1
             let decrementQty = parsedQuantity - 1;
             let itemId = chosenItem.id;
                 console.log(" ");
@@ -126,7 +142,7 @@ function bidAuction(){
                 console.log("    >  --------------------- Your Bid: " + parsedBid);
                 console.log("  ");
                 console.log("------------------------ " + itemId + " -----------------------------------");
-           
+            // I needed to ensure 1. The bid was higher than the Highest Bid. 2. The bid was also higher than the Starting Bid, and 3. The quantity of the item was greater than 0
             if ((chosenItem.highestBid < parsedBid) && (parsedBid > chosenItem.startingBid) && (parsedQuantity > 0)) {
                 connection.query(
                     "UPDATE homework11_db.auctionItems SET ? WHERE ?",[
@@ -151,9 +167,11 @@ function bidAuction(){
                         start();
                     }
                 );
+            // If we're sold out, this happens
             } else if (chosenItem.quantity <= 0){
                 console.log(" ");
                 console.log("OUR APOLOGIES ---> " + chosenItem.itemName + " is currently out of stock.");
+            // If the bid is too low, this happens
             } else {
                 console.log("Your bid was too low. Give it one more try.");
                 console.log(" ");
@@ -163,6 +181,8 @@ function bidAuction(){
     });
 }
 
+// This is the RESTOCK function from a selection within start()
+// First we query the homework11_db.auctionItems table, then we allow the user to input the restock amount via an Inquirer prompt
 function restockAuction() {
     connection.query("SELECT * FROM auctionItems", function(err,results) {
         if (err) throw err;
