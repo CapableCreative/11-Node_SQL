8a32e65cce0d906e04c5266a76812ec163a6f816
Stephen Fox <stephen@capablecreative.com>
2019-10-31 15:17:42 -0500

    Update to stock depletion and restocking - SLF

--- GreatBay.js
+++ GreatBay.js
@@ -21,13 +21,15 @@ function start() {
             name:"postOrBid",
             type:"list",
             message:"Would you like to [POST] a new item, or [BID] on an existing item?",
-            choices:["POST","BID", "QUIT"]
+            choices:["POST","BID","QUIT","RESTOCK"]
         }).then(function(answer) {
         if(answer.postOrBid.toUpperCase()=== "POST") {
             postAuction();
         } else if (answer.postOrBid.toUpperCase()=== "BID") {
             bidAuction();
-        } else {
+        } else if(answer.postOrBid.toUpperCase()=== "RESTOCK") {
+            restockAuction();
+        } else { 
             connection.end();
         }
     });
@@ -47,6 +49,11 @@ function postAuction() {
                 message: "What category would you like to place it in?"
             } , 
             {
+                name: "quantity",
+                type: "input",
+                message: "How many units would you like to stock?"
+            },
+            {
                 name: "startingBid",
                 type: "input",
                 message: "What would you like the starting bid to be? $",
@@ -64,7 +71,8 @@ function postAuction() {
                 itemName: answer.item,
                 category: answer.category,
                 startingBid: answer.startingBid || 0,
-                highestBid: answer.highestBid || 0
+                highestBid: answer.highestBid || 0,
+                quantity: answer.quantity || 0
             },
             function(err) {
                 if (err) throw err;
@@ -106,25 +114,46 @@ function bidAuction(){
             
             let parsedHighestBid = parseInt(chosenItem.highestBid);
             let parsedBid = parseInt(answer.bid);
+            let parsedQuantity = parseInt(chosenItem.quantity);
+            let decrementQty = parsedQuantity - 1;
             let itemId = chosenItem.id;
-             console.log("   Item id: " + itemId);
-             console.log(" ");
-             console.log("   >=< The starting bid is:        " + chosenItem.startingBid);
-             console.log("    ^  The current highest bid is: " + parsedHighestBid);
-             console.log("    >  Your Bid:                   " + parsedBid);
-            if ((chosenItem.highestBid < parsedBid) && (parsedBid > chosenItem.startingBid)) {
+                console.log(" ");
+                console.log("------------------------ ITEM #" + itemId + " -----------------------------");
+                console.log("  ");
+                console.log("   >|< -------------- Starting bid is: " + chosenItem.startingBid);
+                console.log("    ?  ------------ Quantity in stock: " + parsedQuantity);
+                console.log("    ^  ------------ Previous high bid: " + parsedHighestBid);
+                console.log("    >  --------------------- Your Bid: " + parsedBid);
+                console.log("  ");
+                console.log("------------------------ " + itemId + " -----------------------------------");
+           
+            if ((chosenItem.highestBid < parsedBid) && (parsedBid > chosenItem.startingBid) && (parsedQuantity > 0)) {
                 connection.query(
                     "UPDATE homework11_db.auctionItems SET ? WHERE ?",[
+                        {quantity: decrementQty},
+                        {id: itemId}
+                    ],
+                    function(error) {
+                        if (error) throw err;
+                        console.log("!!! ------------------ Your bid successfully placed. -------- ('_') -- !!!");
+                        console.log("!!! ------------------ " + chosenItem.itemName + " New Quantity: " + decrementQty);
+                        console.log(" ");
+                        start();
+                    },
+                     "UPDATE homework11_db.auctionItems SET ? WHERE ?",[
                         {highestBid: parsedBid},
                         {id: itemId}
                     ],
                     function(error) {
                         if (error) throw err;
-                        console.log("!!! ------ Your bid successfully placed. -------- (-_-) !!!");
+                        console.log("!!! ------ Your bid successfully placed. -------- ('_') -- !!!");
                         console.log(" ");
                         start();
                     }
                 );
+            } else if (chosenItem.quantity <= 0){
+                console.log(" ");
+                console.log("OUR APOLOGIES ---> " + chosenItem.itemName + " is currently out of stock.");
             } else {
                 console.log("Your bid was too low. Give it one more try.");
                 console.log(" ");
@@ -132,4 +161,59 @@ function bidAuction(){
                 }
             });
     });
+}
+
+function restockAuction() {
+    connection.query("SELECT * FROM auctionItems", function(err,results) {
+        if (err) throw err;
+        console.log(results);
+        inquirer.prompt([{
+            name: "choice",
+            type: "rawlist",
+            choices: function(){
+                var choiceArray = [];
+                for (var i = 0; i < results.length; i++ ){
+                    choiceArray.push(results[i].itemName);
+                } 
+                return choiceArray;
+            },
+            message: "Which item would you like to RESTOCK?"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many units would you like to stock?"
+            }
+            ]).then(function(answer){
+                var chosenItem;
+                for (var i = 0; i < results.length; i++){
+                    if (results[i].itemName === answer.choice){
+                    chosenItem = results[i];
+                }
+            }
+            let parsedQuantity = parseInt(chosenItem.quantity);
+            let itemId = chosenItem.id;
+                console.log(" ");
+                console.log("------------------------ ITEM #" + itemId + " -----------------------------");
+                console.log("  ");
+                console.log("------------ Quantity in stock: " + answer.quantity);
+                console.log("  ");
+                console.log("------------------------ " + itemId + " -----------------------------------");
+           
+                connection.query(
+                    "UPDATE homework11_db.auctionItems SET ? WHERE ?",[
+                        {quantity: answer.quantity},
+                        {id: itemId}
+                    ],
+                    function(error) {
+                        if (error) throw err;
+                        console.log(" ");
+                        console.log("!!! ------ " + chosenItem.itemName.toUpperCase() + " HAS BEEN RESTOCKED -------- ('_') -- !!!");
+                        console.log(" ");
+                        start();
+                    }
+                )
+            });
+    });
+       
 }
